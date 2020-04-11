2e3863eb92734f1ba3fa37c6ae55417687486b8e
jbguzman <jbguzman@miners.utep.edu>
2020-04-10 21:27:09 -0600

    Made corrections. Removed result array. Populate right array.

--- Java/src/NewSorting.java
+++ Java/src/NewSorting.java
@@ -3,7 +3,7 @@ public class NewSorting {
         if(A.length <= size){ //base case
             quickSort(A); //method call to quicksort
         }
-        else { //recursive
+        else{ //recursive
             int mid = A.length / 2; //find mid of A array
             int[] left = new int[mid]; //left half
             int[] right = new int[A.length - mid]; //right half
@@ -13,7 +13,7 @@ public class NewSorting {
             }
 
             for(int j = 0; j < right.length; j++){ //populate right array
-                right[j] = A[j];
+                right[j] = A[j + mid];
             }
 
             newSorting(left, size); //recursive call
@@ -30,7 +30,7 @@ public class NewSorting {
         if (left == right - 1) { //array with 2 values, no need to partition just compare
             if (A[right] < A[left]) { //swap if the right is less than left
                 int temp = A[right]; //hold value of right temporarily
-                //A[right] = A[left]; //right gets the smaller value of left
+                A[right] = A[left]; //right gets the smaller value of left
                 A[left] = temp; //left become value of temp
             }
             return;
@@ -42,33 +42,36 @@ public class NewSorting {
         }
     }
 
-    private int partition(int[] A, int left, int right){
+    private int partition(int[] A, int left, int right) {
         int pivot = left; //pivot starts at beginning left position
         int less = left + 1; //pivot is assigned to first element so less starts at the next element
         int more = right; //more assigned to the right value
-
+        int temp;
         while(less <= more){
-            while(more > left && A[more] >= pivot){ //more must be greater than left and pivot to move to next index
-                more--; //decrease right side
-            }
 
-            while(less < right && A[less] <= pivot){ //less must be less than the right and pivot to move to next index
-                less++; //increase left side
-            }
-
-            if(less <= more){
-                int temp = A[less]; //if less is greater than more, enter value of less into temp value
-                A[less] = A[more]; //less takes value of more
-                A[more] = temp; //more takes value of less
-                left++;
-                more--;
-            }
+        while (more > left && A[more] >= A[pivot]) { //more must be greater than left and pivot to move to next index
+            more--; //decrease right side
         }
-        return less;
+
+        while (less < right && A[less] <= pivot) { //less must be less than the right and pivot to move to next index
+            less++; //increase left side
+        }
+
+        if (less < more) {
+            temp = A[less]; //if less is greater than more, enter value of less into temp value
+            A[less] = A[more]; //less takes value of more
+            A[more] = temp; //more takes value of less
+            left++;
+            more--;
+        }
+    }
+        temp = pivot;
+        A[left] = A[more];
+        A[more] = temp;
+        return more;
     }
 
     private void mergeSortedHalves(int[] A, int[] left, int[] right){
-        //int[] result = new int[A.length];
         int i = 0; //assigned to left
         int j = 0; //assigned to right
         int k = 0; //assigned to result
@@ -85,20 +88,18 @@ public class NewSorting {
             k++; //keep incrementing index as data is entered
 
             if(i == left.length){ //avoid going out of bounds but right length is larger
-                if(j < right.length){ //avoid right going out of bounds
-                    A[k] = right[i];
-                    //i++; //increment index
-                    break; //end loop
+                while(j < right.length){ //avoid right going out of bounds
+                    A[k] = right[j];
+                    j++;
+                    k++;
                 }
-                i++; //increment index
             }
             if(j == right.length){ //avoid going out of bounds but left length is longer
-                if(i < left.length){ //avoid left going out of bounds
+                while(i < left.length){ //avoid left going out of bounds
                     A[k] = left[i];
-                    //j++; //increment index
-                    break; //end loop
+                    i++;
+                    k++;
                 }
-                j++; //increment index
             }
         }
     }
