7882e572278be070bad792b9136376ff8372682b
jbguzman <jbguzman@miners.utep.edu>
2020-04-13 13:30:11 -0600

    Corrected NewSortingTester.java. All test past.

--- Java/src/NewSortingTester.java
+++ Java/src/NewSortingTester.java
@@ -1,72 +1,166 @@
+//All tests passed
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class NewSortingTester {
 
-    @Test //testing an array with all positive numbers
+    @Test//testing an array with all positive numbers
     public void newSorting1(){
-        NewSorting ns = new newSorting();
-        int[] A = new int[];
-        A = {3, 1, 4, 2};
-        int[] result = new int[];
-        result = {1, 2 ,3, 4};
-        ns.newSorting(A, 2);
+        NewSorting newSorting = new NewSorting();
+
+        int[] A = {12, 5, 54, 20};
+        int[] result = {5, 12, 20, 54};
+
+        System.out.print("Unsorted: ");
+        for(int i = 0; i < A.length; i++){
+            System.out.print(A[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("Sorted: ");
+        for(int i  = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
+        }
+
+        newSorting.newSorting(A, 2);
         assertArrayEquals(result, A);
     }
 
     @Test //testing an array with all negative numbers
     public void newSorting2(){
-        NewSorting ns = new newSorting();
-        int[] A = new int[];
-        A = {-2, -3, -4, -1};
-        int[] result = new int[];
-        result = {-4, -3, -2, -1};
-        ns.newSorting(A, 2);
+        NewSorting newSorting = new NewSorting();
+
+        int[] A = {-2, -3, -4, -1};
+        int[] result = {-4, -3, -2, -1};
+
+        System.out.print("Unsorted: ");
+        for(int i  = 0; i < A.length; i++){
+            System.out.print(A[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("Sorted: ");
+        for(int i  = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
+        }
+
+        newSorting.newSorting(A, 2);
         assertArrayEquals(result, A);
     }
 
     @Test //testing an array with odd length since even length can be divided easily
     public void newSorting3(){
-        NewSorting ns = new newSorting();
-        int[] A = new int[];
-        A = {9, 61, 14, 36, 50, 3, 29};
-        int[] result = new int[];
-        result = {3, 9, 14, 29, 36, 50, 61};
-        ns.newSorting(A, 2);
+        NewSorting newSorting = new NewSorting();
+
+        int[] A = {9, 61, 14, 36, 50, 3, 29};
+        int[] result = {3, 9, 14, 29, 36, 50, 61};
+
+        System.out.print("Unsorted: ");
+        for(int i  = 0; i < A.length; i++){
+            System.out.print(A[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("Sorted: ");
+        for(int i  = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
+        }
+
+        newSorting.newSorting(A, 2);
         assertArrayEquals(result, A);
     }
 
     @Test //testing an array with positive and negative numbers
     public void newSorting4(){
-        NewSorting ns = new newSorting();
-        int[] A = new int[];
-        A = {-3, 7, 11, -39, 20};
-        int[] result = new int[];
-        result = {-39, -3, 7, 11, 20};
-        ns.newSorting(A, 2);
+        NewSorting newSorting = new NewSorting();
+
+        int[] A = {-3, 7, 11, -39, 20};
+        int[] result = {-39, -3, 7, 11, 20};
+
+        System.out.print("Unsorted: ");
+        for(int i = 0; i < A.length; i++){
+            System.out.print(A[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("Sorted: ");
+        for(int i = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
+        }
+
+        newSorting.newSorting(A, 2);
         assertArrayEquals(result, A);
     }
 
     @Test //testing an array with assigned int values input into array
     public void newSorting5(){
-        NewSorting ns = new newSorting();
-        int[] A = new int[];
+        NewSorting newSorting = new NewSorting();
+
         int a = 3;
         int b = 10;
         int c = -9;
         int d = -5;
         int e = 23;
-        A = {a, b, c, d, e};
-        int[] result = new int[];
-        result = {c, d, a, b, e};
-        ns.newSorting(A, 2);
+        int[] A = {a, b, c, d, e};
+        int[] result = {c, d, a, b, e};
+
+        System.out.print("Unsorted: ");
+        for(int i = 0; i < A.length; i++){
+            System.out.print(A[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("Sorted: ");
+        for(int i = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
+        }
+
+        newSorting.newSorting(A, 2);
         assertArrayEquals(result, A);
     }
+
     @Test //testing an array with largest value at beginning array
-    public void newSortingExtra(){
-        NewSorting ns = new newSorting();
-        int[] A = new int[];
-        A = {100, 75, 50, 25};
-        int[] result = new int[];
-        result = {25, 50, 75, 100};
-        ns.newSorting(A, 2);
+    public void newSortingExtra1(){
+        NewSorting newSorting = new NewSorting();
+
+        int[] A = {100, 75, 50, 25};
+        int[] result = {25, 50, 75, 100};
+
+        System.out.print("Unsorted: ");
+        for(int i = 0; i < A.length; i++){
+            System.out.print(A[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("Sorted: ");
+        for(int i = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
+        }
+
+        newSorting.newSorting(A, 1);
+        assertArrayEquals(result, A);
+    }
+
+
+    @Test //testing an array with one value
+    public void newSortingExtra2(){
+        NewSorting newSorting = new NewSorting();
+
+        int[] A = {100};
+        int[] result = {100};
+
+        System.out.print("Unsorted: ");
+        for(int i = 0; i < A.length; i++){
+            System.out.print(A[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("Sorted: ");
+        for(int i = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
+        }
+
+        newSorting.newSorting(A, 1);
         assertArrayEquals(result, A);
     }
 }
